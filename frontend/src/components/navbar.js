dc97796936b9592c46fde0d482d7240851227d71
luispantoja1102 <l20211821@tectijuana.edu.mx>
2024-05-01 12:46:26 -0700

    class t classname

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -2,22 +2,22 @@ import React from "react";
 
 function Navbar(){
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-black">
-            <div class="container">
-                <a class="navbar-brand" href="#">Crud MERN Stack</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-black">
+            <div className="container">
+                <a className="navbar-brand" href="#">Crud MERN Stack</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="colla   pse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/">Inicio</a>
+                <div className="colla   pse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="/">Inicio</a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/agregarusuario">Agregar Usuario</a>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/agregarusuario">Agregar Usuario</a>
                     </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="/editarusuario">Editar Usuario</a>
+                    <li className="nav-item">
+                    <a className="nav-link" href="/editarusuario">Editar Usuario</a>
                     </li>
                 </ul>
                 </div>
